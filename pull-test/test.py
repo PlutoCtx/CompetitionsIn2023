397a0dc808024504a44d4b4c05c6dec327cff86c
PlutoCtx <chentingxian195467@163.com>
2023-05-16 18:57:37 +0800

    pull test

---
+++ pull-test/test.py
@@ -0,0 +1,11 @@
+# @Version: python3.10
+# @Time: 2023/5/16 18:57
+# @Author: MaxBrooks
+# @Email: 15905898514@163.com
+# @File: test.py
+# @Software: PyCharm
+# @User: chent
+
+
+if __name__ == '__main__':
+    print("test01")
